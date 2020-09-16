725c0c260f68fc4cd41995809a23ed73b72007fc
Valera Goretskyi <valeraedu@gmail.com>
2020-09-16 18:35:42 +0300

    updated

--- lesson 3/hw_stack_order.py
+++ lesson 3/hw_stack_order.py
@@ -8,9 +8,6 @@ class Stack():
             stack_name += str(item) + '; '
         return stack_name
 
-    def __init__(self, *args):
-        if len(args):
-            self.main_stack.append(arg for arg in args)
 
     def push(self, element):
         self.main_stack.append(element)
@@ -29,10 +26,6 @@ class Order():
             order_name += str(item) + '; '
         return order_name
 
-    def __init__(self, *args):
-        if len(args):
-            self.main_order.insert(0, (arg for arg in args))
-
     def push(self, element):
         self.main_order.insert(0,element)
         return element
@@ -41,19 +34,22 @@ class Order():
         element = self.main_order.pop()
         return element
 
+
 # stack example
 print('-'*80)
 print('Пример стека')
 new_stack = Stack()
 print (new_stack)
 
-new_stack.push('dict')
-new_stack.push(['1',2,'3'])
+new_stack.push(1)
+new_stack.push(2)
+new_stack.push(3)
 
 print (new_stack)
 
 print (new_stack.pop())
 print (new_stack)
+
 
 # order example
 print('-'*80)
@@ -61,8 +57,9 @@ print('Пример очереди')
 new_order = Order()
 print (new_order)
 
-new_order.push('dict')
-new_order.push(['1',2,'3'])
+new_order.push(1)
+new_order.push(2)
+new_order.push(3)
 
 print (new_order)
 
---
+++ lesson 4/hw_4_1.py
@@ -0,0 +1,87 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+class Persona(ABC):
+
+    @abstractmethod
+    def show_info(self):
+        pass
+
+    @abstractmethod
+    def get_age(self):
+        pass
+
+class Student(Persona):
+
+    def __init__(self, surname, birthday, fakultet, kurs):
+        self.surname = surname
+        self.birthday = datetime.strptime(birthday,'%d/%m/%Y')
+        self.fakultet = fakultet
+        self.kurs = kurs
+
+    def __str__(self):
+        return f"{self.surname}"
+
+    def show_info(self):
+        return f"{self.surname} was born on {self.birthday} and is studying in {self.fakultet} ({self.kurs})"
+
+    def get_age(self):
+        return relativedelta(datetime.now(),self.birthday).years
+
+class Abiturient(Persona):
+
+    def __init__(self, surname, birthday, fakultet):
+        self.surname = surname
+        self.birthday = datetime.strptime(birthday,'%d/%m/%Y')
+        self.fakultet = fakultet
+
+    def __str__(self):
+        return f"{self.surname}"
+
+    def show_info(self):
+        return f"{self.surname} was born on {self.birthday} and is studying in {self.fakultet}"
+
+    def get_age(self):
+        return relativedelta(datetime.now(),self.birthday).years
+
+
+class Teacher(Persona):
+
+    def __init__(self, surname, birthday, fakultet, dolzhnost, staz):
+        self.surname = surname
+        self.birthday = datetime.strptime(birthday,'%d/%m/%Y')
+        self.fakultet = fakultet
+        self.dolzhnost = dolzhnost
+        self.staz = staz
+
+    def __str__(self):
+        return f"{self.surname}"
+
+    def show_info(self):
+        return f"{self.surname} was born on {self.birthday} and teaches {self.fakultet} on {self.dolzhnost} for {self.staz}"
+
+    def get_age(self):
+        return relativedelta(datetime.now(),self.birthday).years
+
+
+stu1 = Student('Goretskyi', '19/08/1991', 'test', 'testik')
+
+abi1 = Abiturient('Komarovskiy', '20/08/1985', 'matematika',)
+
+tea1 = Teacher('Komarov', '20/08/1960', 'fizika', 'professor', '3')
+
+
+person_list = [stu1, abi1, tea1]
+
+for person in person_list:
+    print (person.show_info())
+
+min_age = 20
+max_age = 36
+
+
+
+filtered_list = list(filter(lambda x : x.get_age() >= min_age and x.get_age() <= max_age, person_list))
+
+print(filtered_list)
