d74c58f8758415ab43ccdf326641b5b60944ae46
Valera Goretskyi <valeraedu@gmail.com>
2020-09-17 22:52:03 +0300

    updated

--- lesson 5/hw_5_3.py
+++ lesson 5/hw_5_3.py
@@ -1,4 +1,22 @@
-class MyFile():
+class MyFileWrite():
 
-    def __init__(self):
-        pass
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.f = open(self.filename, self.mode)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.f.close()
+
+    def write_file(self, value):
+        self.f.write(value)
+
+    def read_file(self):
+        result = self.f.read()
+        return result
+
+with MyFileWrite('test.txt', 'r') as myfile:
+    print (myfile.read_file())
